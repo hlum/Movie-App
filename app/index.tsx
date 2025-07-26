ca29ce20a7efbad21d0b34c7bd5cbd264543d465
ラワンアウンピョウ <hlumaungphyo@gmail.com>
2025-07-26 20:13:47 +0900

    extend theme with custom colors

--- app/index.tsx
+++ app/index.tsx
@@ -3,7 +3,7 @@ import { Text, View } from "react-native";
 export default function Index() {
   return (
     <View className="flex-1 justify-center items-center">
-      <Text className="font-bold text-5xl text-blue-500" >Welcome</Text>
+      <Text className="font-bold text-5xl text-accent" >Welcome</Text>
     </View>
   );
 }
